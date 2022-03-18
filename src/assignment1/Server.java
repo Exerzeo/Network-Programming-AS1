cfa3548662adb7364c8656e79146d99dd3cf7798
Hafiz Afiq <hafiz.afiq6079@gmail.com>
2022-03-18 22:14:06 +0800

    adding server class

--- src/assignment1/Server.java
+++ src/assignment1/Server.java
@@ -1,5 +1,30 @@
 package assignment1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
-}
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(12345);
+			System.out.println("Server has started");
+			serverSocket.accept();
+			System.out.println("Client has connected");
+		} catch (IOException e) {
+			System.out.println("Error Occured!");
+		}finally {
+			try {
+			if (serverSocket != null) {
+				
+					serverSocket.close();
+			}
+			} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
